2a873cb71de6e82b540e5917efddd1add15b778a
judhenaoma <judhenaoma@unal.edu.co>
2022-03-21 15:40:35 -0500

    intento 1 clases propuestas en el taller

---
+++ main.py
@@ -0,0 +1,67 @@
+
+
+class Auto:
+
+    cantidadCreados = 0
+
+    def __init__(self, modelo, precio, asientos, marca, motor, registro):
+
+        self.modelo = modelo
+        self.precio = precio
+        self.asientos = asientos
+        self.marca = marca
+        self.motor = motor
+        self.registro = registro
+    
+    def cantidadAsientos(self):
+        return len(self.asientos)
+    
+    def verificarIntegridad(self):
+        if self.registro != self.motor.registro:
+
+            return "Las piezas no son originales"
+        
+        for i in range(len(self.asientos)):
+            if self.asientos.registro != self.registro:
+                return "Las piezas no son originales"
+        
+        return "Auto origina"
+
+
+
+class Asiento:
+
+    def __init___(self, color, precio, registro):
+        self.color = color
+        self.precio = precio
+        self.registro = registro
+
+    
+    def cambiarColor(self, color):
+
+        coloresPermitidos = ["rojo", "verde", "amarillo", "negro", "blanco"]
+
+        if color in coloresPermitidos:
+            self.color = color
+    
+class Motor:
+
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.numeroCilindros = numeroCilindros
+        self.tipo = tipo
+        self.registro = registro
+
+    def cambiarRegistro(self, registro):
+
+        self.registro = registro
+    
+    def asignarTipo(self, tipo):
+
+        tipoPermitido = ["gasolina", "electrico"]
+
+        if tipo in tipoPermitido:
+
+            self.tipo = tipo
+    
+
+
